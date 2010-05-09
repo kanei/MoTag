c0ac52fe08dab5ed690bd4b6bb432f28a0ae1751
root <root@linux-h15g.site>
2010-05-09 12:01:10 +0300

    Added more feature generation methods to csts to crfsuite script

--- tools/crfsuite/example/CoNLL2000/csts_to_crfsuite.py
+++ tools/crfsuite/example/CoNLL2000/csts_to_crfsuite.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: iso-8859-2 -*-
 
 import sys
 import re
@@ -12,7 +12,7 @@ def output_features(fo, seq):
         fs.append('U01=%s' % seq[i-1][0])
         fs.append('U02=%s' % seq[i][0])
         fs.append('U03=%s' % seq[i+1][0])
-        #fs.append('U04=%s' % seq[i+2][0])
+        fs.append('U04=%s' % seq[i+2][0])
         #fs.append('U05=%s/%s' % (seq[i-1][0], seq[i][0]))
         #fs.append('U06=%s/%s' % (seq[i][0], seq[i+1][0]))
 
@@ -30,27 +30,55 @@ def output_features(fo, seq):
         #fs.append('U21=%s/%s/%s' % (seq[i-1][1], seq[i][1], seq[i+1][1]))
         #fs.append('U22=%s/%s/%s' % (seq[i][1], seq[i+1][1], seq[i+2][1]))
 
-        fo.write('%s\t%s\n' % (seq[i][2], '\t'.join(fs)))
+        fs.append('U20=%s' % seq[i-2][2])
+        fs.append('U21=%s' % seq[i-1][2])
+        fs.append('U22=%s' % seq[i][2])
+        fs.append('U23=%s' % seq[i+1][2])
+        fs.append('U24=%s' % seq[i+2][2])
+
+        fs.append('U30=%s' % seq[i-2][3])
+        fs.append('U31=%s' % seq[i-1][3])
+        fs.append('U32=%s' % seq[i][3])
+        fs.append('U33=%s' % seq[i+1][3])
+        fs.append('U34=%s' % seq[i+2][3])
+ 
+        fo.write('%s\t%s\n' % (seq[i][4], '\t'.join(fs)))
     fo.write('\n')
 
 def encode(x):
     x = x.replace('\\', '\\\\')
     x = x.replace(':', '\\:')
     return x
+
+# sets format of tag - only several parts of tag might be used
+def formattag(tag):
+    tag = tag[:2] + tag[4:5] + tag[7:8]
+    return tag
+
     
 fi = sys.stdin
 fo = sys.stdout
 
-s = re.compile('^<s.*?>.*')	# <s .. > ...
-f = re.compile(r"""
+#
+# regular expressions for parsing input data
+#
+sr = re.compile('^<s.*?>.*')	# <s .. > ...
+fr = re.compile(r"""
 		^<[fd].*?>	# <f> or <d> 
 		([^<]*)		# original word
 		<l>([^<]*)	# correct lemma
 		<t>([^<]*)	# correct tag
 		""", re.VERBOSE);	
 
+lr = re.compile('<l>([^<]*)')	# <l> .. - correct lemma
+tr = re.compile('<t>([^<]*)')	# <t> .. - correct tag
+pltr = re.compile('<MMl.*?>([^<]+).*?((?:<MMt.*?>[^<]+)+)') 
+		# <MMl> .. <MMt> .. <MMt> .. - possible lemmas and tags
+mmtr = re.compile('<MMt.*?>([^<]+)')
+		# <MMt> .. - possible tag
 
-d = ('', '', '')
+
+d = ('', '', '', '', '')
 
 seq = [d, d]
 
@@ -65,7 +93,7 @@ for line in fi:
         fo = sys.stdout
     else:
         fo = sys.stderr
-    if s.match(line):
+    if sr.match(line):
          seq.append(d)
          seq.append(d)
 	 output_features(fo, seq)
@@ -73,12 +101,55 @@ for line in fi:
          i = i + 1
          #fo.write('%s\n' % ( line ))
     else:
-         res = f.match(line)
-         if res:
-              tag = res.group(3)[:2] + res.group(3)[3:5] + res.group(3)[7:9]
-              seq.append((encode(res.group(1)), encode(res.group(2)), encode(tag)))
-	      #fo.write('%s\n' % ( res.group(1)))
-	     
+	 #
+	 # match line for correct f object, parse it into lemmas and tags
+	 # 
+         f = fr.match(line)
+         if f:
+		alltags = []
+		plt = pltr.findall(line)
+		lp = ''
+
+		for tags in plt:
+			# save possible lemmas into lp
+			lp = lp + "/" + tags[0]
+
+			mmt = mmtr.findall(tags[1])
+			# append tags from this lemma to common array
+			for tag in mmt:
+				alltags.append(tag)
+
+		# search for common parts of tag and save them to uni
+		uni = alltags[0]
+		for tag in alltags:
+			p = 0
+			for l in tag:
+				if l != uni[p]:
+					uni = uni[:p] + "." + uni[p+1:] 
+				p = p + 1
+
+#			fo.write('%s\n' % (tag))
+
+#		fo.write('%s\n' % (lp))
+
+		# get correct tag and lemma
+		t = formattag(tr.search(line).group(1))
+		l = lr.search(line).group(1)
+		w = f.group(1)
+
+		#get postfix
+		if len(w) > 2:
+			pof = w[-2:] 
+		else:
+			pof = ''
+		fo.write('%s : %s\n' % (w, pof))
+
+              	seq.append((
+			encode(w), 		# word
+			encode(formattag(uni)), # common part of all possible tags
+			encode(lp), 		# possible lemmas
+			encode(pof),		# postfix
+			encode(t)))		# correct tag
 
 #    if not line:
 #        seq.append(d)
