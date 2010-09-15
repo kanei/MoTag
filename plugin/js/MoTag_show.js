903bc4b99dc57011c63b6cea506ccccdfaa9d20d
kanei <marek.vantuch@gmail.com>
2010-09-15 22:24:12 +0200

    Added filters for output tables

---
+++ plugin/js/MoTag_show.js
@@ -0,0 +1,35 @@
+(function($) {
+
+   $(document).ready(function () {
+      var motag_show_table_props =  {
+           col_0: "none",
+           col_1: "select",
+           col_2: "select",
+           col_3: "select",
+           col_4: "select",
+           col_5: "select",
+           col_6: "select",
+           col_7: "select",
+           col_8: "none",
+           col_9: "none",
+           col_10: "none",
+           col_11: "none",
+           col_12: "none",
+           col_13: "none",
+           col_14: "none",
+           col_15: "none",
+           col_16: "none",
+           col_17: "none",
+           filters_row_index: 1, 
+
+       };
+
+      var tf1 = setFilterGrid("motag_show_table", motag_show_table_props);
+   });
+
+  // $('input[name=features[U00]]').attr('checked', true);
+
+
+})(jQuery);
+
+
