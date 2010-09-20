d68bcc949b285738a7242d12cd25192ccadc8eb5
Marek Vantuch <marek.vantuch@gmail.com>
2010-09-20 23:50:56 +0200

    Small diffs

--- data/test.sh
+++ data/test.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+count=7000
 #usage ./test.sh filename
 
 rm $1.test $1.result
-./$1.py <dtest-3-ann.cst >$1.test
+./$1.py -n $count <dtest-3-ann.cst >$1.test
 #/var/mnt/eva/home/users/xv/xvantu00/git/MoTag/test/crfsuite-0.10/bin/crfsuite tag -q -t -m $1.model $1.test
 nohup /var/mnt/eva/home/users/xv/xvantu00/git/MoTag/test/crfsuite-0.10/bin/crfsuite tag -q -t -m $1.model $1.test >$1.result
