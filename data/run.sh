0b509b8460b33c2326167357c4566588788ef4da
Marek Vantuch <marek.vantuch@gmail.com>
2011-05-14 14:57:40 +0200

    Modified run scraipt

--- data/run.sh
+++ data/run.sh
@@ -1,52 +1,34 @@
 #!/bin/sh
 
-# run: nohup ./run.sh -d [pc_id] -t [train_count] -e [test_count] -i [iterations] -p [tag_parts] -r [reg]&
-# default values:        ^pcnlp3    ^10000           ^7000           ^50             ^111110011     ^L1
+# run: nohup ./[computername].sh &
 
-id=pcnlp3
-ntrain=10000
+id=pcnlp5
+ntrain=30000
 ntest=7000
-iter=50
-parts=111110011
-reg=L1
-
-
-while getopts "d:t:e:i:p:r:" optname
-do
-  case "$optname" in 
-  "d")
-  id=$OPTARG
-  ;;
-  "t")
-  ntrain=$OPTARG
-  ;; 
-  "e")
-  ntest=$OPTARG
-  ;; 
-  "i")
-  iter=$OPTARG
-  ;; 
-  "p")
-  parts=$OPTARG
-  ;; 
-  "r")
-  reg=$OPTARG
-  ;;
-  esac
-done
+iter=250
+eps="0.1"
+reg="L2"
+params="-p lbfgs.max_iterations=$iter -p lbfgs.delta=$eps "
+#params="-p algorithm=sgd -p sgd.delta=$eps"
+#WITHOUT u13 U04 U03 U23 U52 U33 U42 U02 U14
 
 #remove existing data
-rm $id.log $id.result $id.train $id.test $id.model $id.test
+rm $id.log $id.result $id.train $id.test $id.model
+
+ulimit -t unlimited
+ulimit -v unlimited
+ulimit -s unlimited
 
 date > $id.log
 #prepare data
 python2.7 $id.py -n $ntrain <train-3-ann.cst > $id.train
 #run training
-/var/mnt/eva/home/users/xv/xvantu00/git/MoTag/test/crfsuite-0.10/bin/crfsuite learn -p regularization=$reg -p lbfgs.max_iterations=$iter -m $id.model $id.train >>$id.log 
+~/git/MoTag/test/crfsuite-0.10/bin/crfsuite learn -p regularization=$reg $params -m $id.model $id.train >>$id.log 
 date >>$id.log
 #prepare data for testing
 python2.7 $id.py -n $ntest <dtest-3-ann.cst > $id.test 
 #run testing
-/var/mnt/eva/home/users/xv/xvantu00/git/MoTag/test/crfsuite-0.10/bin/crfsuite tag -q -t -m $id.model $id.test >$id.result 
+~/git/MoTag/test/crfsuite-0.10/bin/crfsuite tag -q -t -m $id.model $id.test >$id.result 
 date >> $id.result
+mail -s \'$id\' marek.vantuch@gmail.com < $id.log
 
